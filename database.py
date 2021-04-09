6bc23f3aedf6fda14f659231e9f6bf582009a9e0
KyblaHan <liberiska@gmail.com>
2021-04-09 23:33:40 +0300

    Наполнил БД, изменил разрешение проги на 1280*1024

--- database.py
+++ database.py
@@ -1,45 +1,64 @@
+# -*- coding: utf-8 -*- #
 import sqlite3
 import pandas as pd
 from collections import Counter
+from transliterate import translit
 
 conn = sqlite3.connect('ProjectData/SystemFiles/knowledge_base.db')
 cursor = conn.cursor()
 
-def import_db(file):
-    data = pd.read_csv(file, sep=';', thousands=",", low_memory=False)
-
-
-def transliteration(text):
-    cyrillic = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-    latin = 'a|b|v|g|d|e|e|zh|z|i|i|k|l|m|n|o|p|r|s|t|u|f|kh|tc|ch|sh|shch||y||e|iu|ia|A|B|V|G|D|E|E|Zh|Z|I|I|K|L|M|N|O|P|R|S|T|U|F|Kh|Tc|Ch|Sh|Shch||Y||E|Iu|Ia'.split('|')
-    return text.translate({ord(k):v for k,v in zip(cyrillic,latin)})
 
 def cells_types_append(file):
     data = pd.read_csv(file, sep=';', thousands=",", low_memory=False)
     classes = list(Counter(data["Label"]))
     input_db = []
-    i =0
+    i = 0
     for cl in classes:
-        input_db.append((str(i),cl))
-        i+=1
+        input_db.append((str(i), cl))
+        i += 1
     cursor.executemany("insert into CellTypes values  (? ,?);", input_db)
     conn.commit()
 
+
 def sign_types_append(file):
-    data = pd.read_csv(file, sep=';', thousands=",", low_memory=False,encoding="ascii")
+    data = pd.read_csv(file, sep=';', thousands=",", low_memory=False)
     del data["path_to_cell"]
     del data["Label"]
     input_db = []
     i = 0
     for col in data.columns:
-        input_db.append((str(i), transliteration(col)))
-        print(transliteration(col))
-        i+=1
+        input_db.append((str(i), col))
+        i += 1
+    cursor.executemany("insert into SignTypes values  (? ,?);", input_db)
+    conn.commit()
 
-    # cursor.executemany("insert into SignTypes values  (? ,?);", input_db)
-    # conn.commit()
 
-# import_db("ProjectData/Data/all_append.csv")
-sign_types_append("ProjectData/Data/all_append.csv")
+def cells_append(file):
+    data = pd.read_csv(file, sep=';', thousands=",", low_memory=False)
+    paths = data["path_to_cell"]
+    labels = data["Label"]
+    input_db = []
+    uniq_labels = list(Counter(labels))
+    for i in range(0, len(paths)):
+        input_db.append((str(i), paths[i], uniq_labels.index(labels[i])))
+    cursor.executemany("insert into Cells values  (? ,?,?);", input_db)
+    conn.commit()
 
+
+def sings_append(file):
+    data = pd.read_csv(file, sep=';', thousands=",", low_memory=False)
+    del data["path_to_cell"]
+    del data["Label"]
+    input_db = []
+    i = 0
+    for index, row in data.iterrows():
+        j = 0
+        for r in row:
+            input_db.append((i, index, j, r))
+            i += 1
+            j += 1
+    cursor.executemany("insert into Sings values  (?, ?, ?, ?);", input_db)
+    conn.commit()
+
+# sings_append("ProjectData/Data/all_append_.csv")
 conn.close()
